7d578cfa146954e17463d945631853bfd2eb93c9
Zack Chase <zack@distelli.com>
2015-10-28 08:16:23 -0700

    Updated Tutorial

--- spec/helloWorldSpec.js
+++ spec/helloWorldSpec.js
@@ -1,21 +1,10 @@
-var request = require("request");
+var request = require('request');
 
-var base_url = "http://localhost:3000/"
-
-describe("Hello World Server", function() {
-  describe("GET /", function() {
-    it("returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-    });
-
-    it("returns Hello World", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(body).toBe("Hello World");
-        done();
-      });
-    });
-  });
+describe('my test suite',function(){
+	it("should respond with hello world", function(done) {
+	  request("http://localhost:3000", function(error, response, body){
+	    expect(body).toEqual("Hello World");
+	    done();
+	  });
+	});
 });
