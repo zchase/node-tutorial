87da11754d1f218e6393b36eaf064438504f3419
Zack Chase <zack@distelli.com>
2015-10-29 10:49:52 -0700

    Change to App.js to see if Jasmine test failure will created failed Build

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var exports = module.exports = {};
 
 app.get('/', function(req, res){
-	res.send('Hello World');
+	res.send('Hell World');
 });
 
 var server = app.listen(3000, function(){
